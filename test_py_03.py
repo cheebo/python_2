cd474858df2d6c51e6d82e72a2e6a4edeff5de4d
Sergei Chebotar <sergey@Sergeis-MacBook-Pro.local>
2024-10-15 23:25:51 +0500

    implement t3

---
+++ test_py_03.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from py_03 import priceLess20
+
+
+class TestPriceLess20(TestCase):
+    def test_price_less20(self):
+        items = {
+            'milk15': {'name': 'молоко 1.5%', 'count': 34, 'price': 89.9},
+            'cheese': {'name': 'сыр молочный 1 кг.', 'count': 12, 'price': 990.9},
+            'sausage': {'name': 'колбаса 1 кг.', 'count': 122, 'price': 1990.9}
+        }
+        self.assertEqual(priceLess20(items), { 'milk15': False, 'cheese': True, 'sausage': False})
+
+
